d062ac38c1b7c878112baec71ac61ed002ab8be9
Vinod K Tomy <vinod@teamevoque.com>
2017-12-30 16:26:24 +0530

    filter

--- js/main.js
+++ js/main.js
@@ -1,14 +1,74 @@
+var filterpublicationValue = "all";
+var filtercountryValue = "all";
+var filterLangValue = "all";
 
   $(document).ready(function() {
+    $('select').niceSelect();
+    
+    $('#qf-filter-lang').on('change', function() {
+      filterLangValue = this.value;
+      $(".qf-highlights-item-wrapper").fadeOut();
+      contentFilter();
+    })
+    $('#qf-pub').on('change', function() {
+      filterpublicationValue = this.value;
+      $(".qf-highlights-item-wrapper").fadeOut();
+      contentFilter();
+    })
+    $('#qf-filter-country').on('change', function() {
+      filtercountryValue = this.value;
+      $(".qf-highlights-item-wrapper").fadeOut();
+      contentFilter();
+    })
+  });
 
-    $('#qf-sort').on('change', function() {
-      if(this.value == "all"){
-        $(".qf-highlights-item-wrapper").fadeIn();
-      }
-      else{
-        $(".qf-highlights-item-wrapper").fadeOut();
-        $("."+this.value+"").fadeIn();
+function contentFilter(){
+  if(filterLangValue == "all" && filterpublicationValue == "all" && filtercountryValue == "all"){
+    $(".qf-highlights-item-wrapper").fadeIn();
+  }
+  else if(filterLangValue != "all" && filterpublicationValue == "all" && filtercountryValue == "all"){
+    $("."+filterLangValue+"").fadeIn();
+  }
+  else if(filterLangValue == "all" && filterpublicationValue != "all" && filtercountryValue == "all"){
+    $(".qf-highlights-item-wrapper").each(function(i, value) {
+      if($(this).attr("data-filter-lang") == filterpublicationValue){
+        $(this).fadeIn();
       }
     })
-
-  });
+  }
+  else if(filterLangValue == "all" && filterpublicationValue == "all" && filtercountryValue != "all"){
+    $(".qf-highlights-item-wrapper").each(function(i, value) {
+      if($(this).attr("data-filter-country") == filtercountryValue){
+        $(this).fadeIn();
+      }
+    })
+  }
+  else if(filterLangValue != "all" && filterpublicationValue != "all" && filtercountryValue == "all"){
+    $("."+filterLangValue+"").each(function(i, value) {
+      if($(this).attr("data-filter-lang") == filterpublicationValue){
+        $(this).fadeIn();
+      }
+    })
+  }
+  else if(filterLangValue == "all" && filterpublicationValue != "all" && filtercountryValue != "all"){
+    $(".qf-highlights-item-wrapper").each(function(i, value) {
+      if(($(this).attr("data-filter-lang") == filterpublicationValue) && ($(this).attr("data-filter-country") == filtercountryValue)){
+        $(this).fadeIn();
+      }
+    })
+  }
+  else if(filterLangValue != "all" && filterpublicationValue == "all" && filtercountryValue != "all"){
+    $("."+filterLangValue+"").each(function(i, value) {
+      if($(this).attr("data-filter-country") == filtercountryValue){
+        $(this).fadeIn();
+      }
+    })
+  }
+  else{
+    $("."+filterLangValue+"").each(function(i, value) {
+      if(($(this).attr("data-filter-lang") == filterpublicationValue) && ($(this).attr("data-filter-country") == filtercountryValue)){
+        $(this).fadeIn();
+      }
+    })
+  }  
+}
